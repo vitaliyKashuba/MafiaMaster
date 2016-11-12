547f97d9ab3781ffc98aa479852ae2bd53d2c71e
vitaliyKashuba <k.vitaliy1994@gmail.com>
2016-11-12 18:26:56 +0200

    add name input tryed material design. unsucess

--- app/src/main/java/vitaliy94/mafiamaster/RolesRandomizeActivity.java
+++ app/src/main/java/vitaliy94/mafiamaster/RolesRandomizeActivity.java
@@ -1,10 +1,17 @@
 package vitaliy94.mafiamaster;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 
-public class RolesRandomizeActivity extends AppCompatActivity {
+public class RolesRandomizeActivity extends AppCompatActivity
+{
+
+    TextInputLayout textInputLayout;
+    EditText inputName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,5 +19,12 @@ public class RolesRandomizeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roles_randomize);
         getSupportActionBar().hide();
+
+        textInputLayout = (TextInputLayout)findViewById(R.id.inputNameLayout);
+        inputName = (EditText)textInputLayout.findViewById(R.id.inputName);
+
+        textInputLayout.setHint(getString(R.string.inputNameHint));
+
     }
+
 }
