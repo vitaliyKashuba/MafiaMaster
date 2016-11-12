7062ed464d3823b62af4b8e1bbc1d6c9260165d0
vitaliyKashuba <k.vitaliy1994@gmail.com>
2016-11-12 14:39:12 +0200

    add onClicks for first line

--- app/src/main/java/vitaliy94/mafiamaster/MainActivity.java
+++ app/src/main/java/vitaliy94/mafiamaster/MainActivity.java
@@ -2,12 +2,40 @@ package vitaliy94.mafiamaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    int playersCount = 10;
+    TextView tvPlayersCount;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TextView tvPlayersCount = (TextView)findViewById(R.id.playersCount);
+        //tvPlayersCount.setText(String.valueOf(playersCount));
     }
+
+    public void onLessPlayersClick(View view)
+    {
+        playersCount--;
+        setPlayersCountText(String.valueOf(playersCount));
+    }
+
+    public void onMorePlayersClick(View view)
+    {
+        playersCount++;
+        setPlayersCountText(String.valueOf(playersCount));
+    }
+
+    public void setPlayersCountText(String text)
+    {
+        TextView tvPlayersCount = (TextView)findViewById(R.id.playersCount);
+        tvPlayersCount.setText(text);
+    }
+
 }
