2236e0c33c2b4bc2320c7fe484713cc9923feb6e
vitaliyKashuba <k.vitaliy1994@gmail.com>
2016-11-26 20:15:10 +0200

    remove thrash and 'magic'

--- app/src/main/java/vitaliy94/mafiamaster/GameActivity.java
+++ app/src/main/java/vitaliy94/mafiamaster/GameActivity.java
@@ -1,34 +1,27 @@
 package vitaliy94.mafiamaster;
 
 import android.graphics.Color;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeMap;
 
-public class GameActivity extends AppCompatActivity
+public class GameActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener
 {
     ListView listView;
 
-    //HashMap<String, Roles> players; //treeMap beomes hashMap after intent. some kind of magic
-
     ArrayList players;
     PlayersAdapter adapter;
+    SwipeRefreshLayout srLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -37,45 +30,10 @@ public class GameActivity extends AppCompatActivity
         setContentView(R.layout.activity_game);
         getSupportActionBar().hide();
 
-        //ListView listView = (ListView)findViewById(R.id.lV);
+        srLayout = (SwipeRefreshLayout) findViewById(R.id.activity_game);
+        srLayout.setOnRefreshListener(this);
 
-        //players = (HashMap<String, Roles>)getIntent().getSerializableExtra("players");
         players = (ArrayList<Player>)getIntent().getSerializableExtra("players");
-
-        /*Set<String> p = players.keySet();                       //
-        String[] playersRoles = new String[players.size()];     //TODO refactor it. later
-        Object[]po = p.toArray();                               //
-
-        for(int i = 0; i < p.size(); i++)
-        {
-            playersRoles[i] = fillWithSpaces(String.valueOf(po[i]), 12) + " " + getString(players.get(String.valueOf(po[i])).getResId());
-        }
-
-        ArrayAdapter<String> ad = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, playersRoles);
-        listView.setAdapter(ad);
-
-        registerForContextMenu(listView);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-            {
-                TextView tv = (TextView) view;
-                String txt = tv.getText().toString();
-                if(!txt.contains("DEAD"))
-                {
-                    tv.setText(txt + " DEAD");
-                    tv.setBackgroundColor(Color.RED);
-                }
-                else
-                {
-                    tv.setText(txt.substring(0, txt.length()-5));
-                    tv.setBackgroundColor(Color.WHITE);
-                }
-                Log.d("html", tv.getText().toString());
-            }
-        });*/
 
         adapter = new PlayersAdapter(this, players);
 
@@ -124,29 +82,13 @@ public class GameActivity extends AppCompatActivity
         }
     }
 
-    /**
-     * fill string with spaces
-     * used to align collumns
-     *
-     * doesn't work :(
-     *
-     * @return
-     */
-    String fillWithSpaces(String str, int toSize)
+    @Override
+    public void onRefresh()
     {
-        if(str.length()>=toSize)
-        {
-            return str;
-        }
-        else
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.append(str);
-            for (int i = 0; i < toSize-str.length(); i++)
-            {
-                sb.append(" ");
-            }
-            return sb.toString();
-        }
+
+
+
+        srLayout.setRefreshing(false);
     }
+
 }
